e668703e0ae0b1eff150c425ba094c00d66f1784
Karan Patel <98628887+KP4RU@users.noreply.github.com>
2022-08-22 13:25:36 -0400

    Update failing_tool.py
    path fixes

--- .lift/failing_tool.py
+++ .lift/failing_tool.py
@@ -13,14 +13,18 @@ def emit_applicable():
     print("true")
 
 def run():
-    print('[{\"type\" : \"lift test\",\"message\" : \"testing lift\", \"file\" : \".lift/failing_tool.py\",\"line\" : 1}]')
+    print('[{\"type\" : \"lift test dev1\",\"message\" : \"testing lift in dev1\", \"file\" : \".lift/failing_tool.py\",\"line\" : 1}]')
     # raise Exception('Feeling under the weather')
 
 def main():
     parser = argparse.ArgumentParser(description='Failing Tool')
+    parser.add_argument('path', metavar='PATH', help='Path to code')
+    parser.add_argument('commit_hash', metavar='HASH', help='Commit hash')
     parser.add_argument('command', metavar='COMMAND', help='Command')
 
     args = parser.parse_args()
+
+    path = args.path
 
     command = args.command
 
